e43bcb1b341a9e889f226fd96001cb1e7ab87c9d
Blair Nangle <blair.nangle@gmail.com>
2020-04-04 10:28:24 +0100

    GH-3 Set 15-minute max timeout for Tweepy API
    - Wait up tp 15 minutes (900 seconds) for Twitter to respond

--- authenticate.py
+++ authenticate.py
@@ -5,4 +5,4 @@ def authenticate(consumer_key: str, consumer_secret: str, access_key: str, acces
     auth = tweepy.OAuthHandler(consumer_key=consumer_key, consumer_secret=consumer_secret)
     auth.set_access_token(key=access_key, secret=access_secret)
 
-    return tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    return tweepy.API(auth, timeout=900, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
